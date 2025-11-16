55d13738dcf2b5558d7498c36251dbb146364de9
Ezardy <zaharanikinmai@gmail.com>
2025-11-16 20:15:53 +0400

    correct urls

--- src/scripts/web-api/server-game.ts
+++ src/scripts/web-api/server-game.ts
@@ -135,7 +135,7 @@ export class ServerGame
 	}
 
 	public	open() : void {
-		this._lobbyWs = new WebSocket(`${/**process.env.SERVER_WS_URL ?? **/"ws://localhost:4000/ws"}/lobby?sId=${this._sessionId}`);
+		this._lobbyWs = new WebSocket(`wss://${window.location.host}/ws/lobby?sId=${this._sessionId}`);
 
 		this._lobbyWs.onopen = () => {
 			console.log("Connected to Lobby WebSocket ");
