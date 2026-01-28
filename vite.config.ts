123051e30027cc97019a3d08be6730311207409b
Ezardy <zaharanikinmai@gmail.com>
2026-01-28 12:59:55 +0400

    vite.config.ts takes variables during docker compose build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import path from "path";
 
-export default defineConfig({
-  base: process.env.VITE_APP_BASE,
-  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-  server: {
-    port: 3000,
-    host: true,
-  }
-});
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    base: process.env.VITE_HOSTNAME,
+    resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+    server: {
+      port: 3000,
+      host: true,
+    }
+  });
+}
